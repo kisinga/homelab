d79db8d03e9c987675d61e21bdfc26d572fb17b0
kisinga <stevekisinga@gmail.com>
2025-06-01 19:14:28 +0300

    fix: update env file path in container runner script

--- scripts/container-runner.sh
+++ scripts/container-runner.sh
@@ -5,7 +5,7 @@ for file in /srv/containers/*; do
   case "$file" in
     *.yml)
       /usr/bin/docker compose \
-        --env-file /srv/homelab/.env \
+        --env-file /srv/.env \
         --project-directory /srv/containers \
         -f "$file" up -d
       ;;
